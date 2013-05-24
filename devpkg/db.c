399856235509c6db293ecd43590d8d8dfd89b946
swrangsar <swrangsar@spannbhakt-desktop.(none)>
2013-05-24 13:47:37 +0530

    askfjkq905

---
+++ devpkg/db.c
@@ -0,0 +1,57 @@
+#include <unistd.h>
+#include <apr_errno.h>
+#include <apr_file_io.h>
+
+#include "db.h"
+#include "bstrlib.h"
+#include "dbg.h"
+
+static FILE *DB_open(const char *path, const char *mode)
+{
+    return fopen(path, mode);
+}
+
+static void DB_close(FILE *db)
+{
+    fclose(db);
+}
+
+static bstring DB_load()
+{
+    FILE *db = NULL;
+    bstring data = NULL;
+    
+    db = DB_open(DB_FILE, "r");
+    check(db, "Failed to open database: %s", DB_FILE);
+    
+    data = bread((bNread)fread, db);
+    check(data, "Failed to read from db file: %s", DB_FILE);
+    
+    DB_close(db);
+    return data;
+    
+error:
+    if(db) DB_close(db);
+    if(data) bdestroy(data);
+    return NULL;
+}
+
+int DB_update(const char *url)
+{
+    if(DB_find(url)) {
+        log_info("Already recorded as installed: %s", url);
+    }
+    
+    FILE *db = DB_open(DB_FILE, "a+");
+    check(db, "Failed to open DB file: %s", DB_FILE);
+    
+    bstring line = bfromcstr(url);
+    bconchar(line, '\n');
+    int rc = fwrite(line->data, blength(line), 1, db);
+    check(rc == 1, "Failed to append to the db.");
+    
+    return 0;
+error:
+    if (db) DB_close(db);
+    return -1;
+}
